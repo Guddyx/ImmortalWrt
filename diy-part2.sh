8a8cca80c2375bc51efd83c1ac4ed497fbe981ce
Guddyx <55354502+Guddyx@users.noreply.github.com>
2024-02-07 23:11:12 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,9 +14,9 @@
 sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generate
 
 # 移除要替换的包
-rm -rf feeds/luci/applications/luci-app-mosdns
+#rm -rf feeds/luci/applications/luci-app-mosdns
 
 # 添加luci-app-mosdns
-#git clone https://github.com/sbwml/luci-app-mosdns.git mosdns
-#cp -rf mosdns/luci-app-mosdns feeds/luci/applications/luci-app-mosdns
-#rm -rf mosdns
+git clone https://github.com/sbwml/luci-app-mosdns.git mosdns
+cp -rf mosdns/luci-app-mosdns feeds/luci/applications/luci-app-mosdns
+rm -rf mosdns
