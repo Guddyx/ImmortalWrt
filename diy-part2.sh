43e288069d9ef47bc8bee0ee2c831c78f86fd9ef
Guddyx <55354502+Guddyx@users.noreply.github.com>
2024-02-10 00:00:58 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,9 +14,19 @@
 sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generate
 
 # 移除要替换的包
-#rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds\luci\applications\luci-app-passwall
 
 # 添加luci-app-mosdns
 git clone https://github.com/sbwml/luci-app-mosdns.git mosdns
 cp -rf mosdns/luci-app-mosdns feeds/luci/applications/luci-app-mosdns
 rm -rf mosdns
+
+#添加luci-app-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall-packages.git passwall_packages
+git clone https://github.com/xiaorouji/openwrt-passwall.git passwall
+cp -rf feeds\passwall\luci-app-passwall feeds/luci/applications/luci-app-passwall
+cp -rf feeds\passwall\luci-app-passwall feeds/luci/applications/luci-app-passwall
+cp -rf feeds\passwall_packages\gn feeds\packages\net
+cp -rf feeds\passwall_packages\ssocks feeds\packages\net
+rm -rf passwall_packages
+rm -rf passwall
