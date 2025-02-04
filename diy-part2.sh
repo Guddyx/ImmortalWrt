0eb59a9aa8ea7e4b98e7def681cf25ddf96c8754
Guddyx <55354502+Guddyx@users.noreply.github.com>
2025-02-04 23:49:52 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -42,17 +42,17 @@ git clone https://github.com/xiaorouji/openwrt-passwall-packages.git passwall_pa
 cp -rf passwall_packages/gn package/feeds/packages/gn
 
 # 移除要替换的包
-#rm -rf feeds/luci/applications/luci-app-passwall
-#rm -rf feeds/packages/net/chinadns-ng
-#rm -rf feeds/packages/net/shadowsocks-libev
-#rm -rf feeds/packages/net/simple-obfs
+rm -rf feeds/luci/applications/luci-app-passwall
+rm -rf feeds/packages/net/chinadns-ng
+rm -rf feeds/packages/net/shadowsocks-libev
+rm -rf feeds/packages/net/simple-obfs
 
 # 添加luci-app-passwall
-#git clone https://github.com/xiaorouji/openwrt-passwall.git passwall
-#cp -rf passwall/luci-app-passwall feeds/luci/applications/luci-app-passwall
-#cp -rf passwall_packages/chinadns-ng feeds/packages/net/chinadns-ng
-#cp -rf passwall_packages/ssocks feeds/packages/net/ssocks
-#cp -rf passwall_packages/simple-obfs feeds/packages/net/simple-obfs
+git clone https://github.com/xiaorouji/openwrt-passwall.git passwall
+cp -rf passwall/luci-app-passwall feeds/luci/applications/luci-app-passwall
+cp -rf passwall_packages/chinadns-ng feeds/packages/net/chinadns-ng
+cp -rf passwall_packages/ssocks feeds/packages/net/ssocks
+cp -rf passwall_packages/simple-obfs feeds/packages/net/simple-obfs
 #rm -rf passwall_packages
 #rm -rf passwall
 
