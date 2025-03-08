1e56c91c12d2c64f822e1de739f63e8f826cb328
Guddyx <55354502+Guddyx@users.noreply.github.com>
2025-03-08 23:21:05 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -46,6 +46,7 @@ rm -rf feeds/packages/net/xray-core
 rm -rf feeds/packages/microsocks
 rm -rf feeds/packages/naiveproxy
 rm -rf feeds/packages/net/v2ray-core
+rm -rf feeds/packages/net/v2ray-geodata
 
 # 添加luci-app-passwall
 #git clone https://github.com/xiaorouji/openwrt-passwall.git passwall
@@ -67,6 +68,7 @@ cp -rf passwall_packages/microsocks feeds/packages/net/microsocks
 cp -rf passwall_packages/naiveproxy feeds/packages/net/naiveproxy
 cp -rf passwall_packages/trojan-plus feeds/packages/net/trojan-plus
 cp -rf passwall_packages/v2ray-core feeds/packages/net/v2ray-core
+cp -rf passwall_packages/v2ray-geodata feeds/packages/net/v2ray-geodata
 #rm -rf passwall_packages
 #rm -rf passwall
 
