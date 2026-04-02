16080bfc2c4942574df058aa3071f7153c9eaeb4
Guddyx <55354502+Guddyx@users.noreply.github.com>
2026-04-02 08:41:29 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -40,9 +40,9 @@ rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/lang/golang
 
 # 更新为LEDE-rust包
-rm -rf feeds/packages/lang/rust
-git clone https://github.com/coolsnowwolf/packages LEDE-packages
-cp -rf LEDE-packages/lang/rust feeds/packages/lang/rust
+#rm -rf feeds/packages/lang/rust
+#git clone https://github.com/coolsnowwolf/packages LEDE-packages
+#cp -rf LEDE-packages/lang/rust feeds/packages/lang/rust
  
 # 移除要替换的包
 rm -rf feeds/luci/applications/luci-app-passwall
